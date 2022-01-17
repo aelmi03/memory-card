24cd7e765ecfb4426919dcf3a030d856280c1862
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-01-16 18:41:19 -0800

    Created my AnimeCharacter array in the utils folder which will hold all the characters, and there is a total of 22 characters inside the array

---
+++ src/Utils/AnimeCharacters.js
@@ -0,0 +1,137 @@
+import uniqid from "uniqid";
+
+const animeCharacters = [
+  {
+    name: "Kakashi Hatake",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Kakashi-Hatake-Naruto.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Saitama",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Saitama-One-Punch-Man.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Goku",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Goku-Dragon-Ball-Z.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Ken Kaneki",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Ken-Kaneki-Tokyo-Ghoul.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Itachi Uchiha",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Itachi-Uchiha-Naruto.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Guts",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Guts-Berserk.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Mikasa Ackerman",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Mikasa-Ackerman-Attack-on-Titan-2.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Roronoa Zoro",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Roronoa-Zoro-One-Piece.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Sakata Gintoki",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Sakata-Gintoki-Gintama.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Edward Elric",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Edward-Elric-Fullmetal-Alchemist-Brotherhood.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Light Yagami",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Light-Yagami-Death-Note.jpg",
+    id: uniqid(),
+  },
+  {
+    name: " Eren Yeager",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Eren-Yeager-Attack-on-Titan.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Okabe Rintaro",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Okabe-Rintaro-SteinsGate.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Naruto Uzumaki",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Naruto-Uzumaki-Naruto.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Monkey D. Luffy",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Monkey-D.-Luffy-One-Piece.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Killua Zoldyck",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Killua-Zoldyck-Hunter-x-Hunter.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "L Lawliet",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/L-Lawliet-Death-Note.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Satoru Gojo",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Satoru-Gojo-Jujutsu-Kaisen.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Levi Ackerman ",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Levi-Ackerman-Attack-on-Titan.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Lelouch Lamperouge",
+    imageURL:
+      "https://www.animesoulking.com/wp-content/uploads/2021/12/Lelouch-Lamperouge-Code-Geass.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Makunouchi Ippo",
+    imageURL: "https://i1.sndcdn.com/artworks-000343540824-726wyi-t500x500.jpg",
+    id: uniqid(),
+  },
+  {
+    name: "Eikichi Onizuka",
+    imageURL:
+      "https://occ-0-990-987.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABZ2XeH-7hzErGPlz9D0huYD-sDV2P-MRDH-OMXKS233tNTy_j4llZPtMGHzirJ152jiHFsF4R1IKuP0aT6jEZBBoKfsR.jpg?r=a8d",
+    id: uniqid(),
+  },
+];
+
+export default animeCharacters;
